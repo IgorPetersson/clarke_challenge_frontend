b49a89a75dde6ea72cd58153dd3357fd503b0b68
igor <igorpeterssontrab@gmail.com>
2022-01-30 11:47:08 -0400

    feat: added Button component

---
+++ src/components/Button/index.jsx
@@ -0,0 +1,11 @@
+import { Container } from "./styles"
+
+const Button = ({children, onClick}) => {
+    return(
+        <Container onClick={onClick}>
+            {children}
+        </Container>
+    )
+}
+
+export default Button
---
+++ src/components/Button/styles.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+    width: 100%;
+    height: 55px;
+    border-radius: 8px;
+    background-color: white;
+    color: black;
+    text-transform: uppercase;
+    border: 2px solid black;
+    cursor: pointer;
+
+`
