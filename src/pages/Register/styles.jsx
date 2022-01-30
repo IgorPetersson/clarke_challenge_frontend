ace234b362f67e460e15ece97975596f9203f7ac
igor <igorpeterssontrab@gmail.com>
2022-01-30 12:36:39 -0400

    feat: added Register page

---
+++ src/pages/Register/styles.jsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    height: 100vh;
+    width: 100vw;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`
+
+export const ContainerForm = styled.form`
+    height: 380px;
+    
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+`
+
+export const Acount = styled.div``
